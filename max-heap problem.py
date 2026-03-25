4de124e0d3acea55182fdec36233ad0c5b5082cf
hrithik-cse <Hritik.raj.cs28@iilm.edu>
2026-03-25 23:23:18 +0530

    max-heap problem.py

---
+++ max-heap problem.py
@@ -0,0 +1,20 @@
+import heapq
+
+def min_operations(arr):
+    total_sum = sum(arr)
+    target = total_sum / 2
+    
+    max_heap = [-x for x in arr]
+    heapq.heapify(max_heap)
+    
+    current_sum = total_sum
+    operations = 0
+    
+    while current_sum > target:
+        largest = -heapq.heappop(max_heap)
+        half = largest / 2
+        current_sum -= half
+        heapq.heappush(max_heap, -half)
+        operations += 1
+    
+    return operations
